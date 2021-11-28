a729e39ccf053b61d266ceb3a8c24ad99e562165
Dulcinha <https://github.com/Dulcinha>
2021-11-28 20:32:43 -0300

    Atividade 10 implementado

--- projeto/src/main/java/control/AlunoControle.java
+++ projeto/src/main/java/control/AlunoControle.java
@@ -1,5 +1,6 @@
 package control;
 
+import java.awt.datatransfer.SystemFlavorMap;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -10,19 +11,59 @@ import model.Aluno;
 import javax.persistence.EntityManagerFactory;
 public class AlunoControle {
 	private EntityManager em;
+	private EntityManagerFactory emf;
 
 	public AlunoControle() {
-		EntityManager emf = (EntityManager) Persistence.createEntityManagerFactory("clinica");
-		em = ((EntityManagerFactory) emf).createEntityManager();
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("projeto"); 
+		EntityManager em = emf.createEntityManager(); 
 	}
-	public void inserir(Aluno objeto) {}
-	public void alterar(Aluno objeto) {}
-	public void excluir(Aluno objeto) {}
-	public void excluirPorId(Integer id) {}
+	public void inserir(Aluno objeto) {
+		try {
+            em.getTransaction().begin();
+            em.persist(objeto);
+            em.getTransaction().commit();
+            em.close();
+            emf.close();
+         } catch (Exception ex) {
+            ex.printStackTrace();
+            em.getTransaction().rollback();
+         }
+	}
+	public void alterar(Aluno objeto) {
+		try {
+            em.getTransaction().begin();
+            em.merge(objeto);
+            em.getTransaction().commit();
+         } catch (Exception ex) {
+            ex.printStackTrace();
+            em.getTransaction().rollback();
+         }
+	}
+	public void excluir(Aluno objeto) {
+		try {
+            em.getTransaction().begin();
+            em.remove(objeto);
+            em.getTransaction().commit();
+         } catch (Exception ex) {
+            ex.printStackTrace();
+            em.getTransaction().rollback();
+         }
+	}
+	public void excluirPorId(Integer id) {
+		try {
+            Aluno objeto = buscarPorId(id);
+            excluir(objeto);
+         } catch (Exception ex) {
+            ex.printStackTrace();
+         }
+	}
 	public Aluno buscarPorId(Integer id) {
-		return null;}
+		
+		return em.find(Aluno.class, id)
+;}
 	public List<Aluno> buscarTodos() {
-		return null;}
+		String nomeClasse = Aluno.class.getName();
+		return em.createQuery("FROM " + nomeClasse).getResultList();}
 	//outros métodos de busca que achar 
 	
 	
--- projeto/src/main/java/view/Programa.java
+++ projeto/src/main/java/view/Programa.java
@@ -1,35 +1,46 @@
 package view;
 import java.sql.Date;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.persistence.EntityManager; 
 import javax.persistence.EntityManagerFactory; 
 import javax.persistence.Persistence;
 
+import control.AlunoControle;
 import model.Aluno;
 
 public class Programa {
 	public static void main(String[] args) {
 		
-		Aluno aluno1 = new Aluno(null, "Fabiana Reis Fernandes", "2000-04-05", "178889912", "1", "fabireis@gmail.com","F");
-		Aluno aluno2 = new Aluno(null, "Maria Joana Fagundes", "2000-10-11",   "201119224", "1", "mariaj2@gmail.com","F");
-		Aluno aluno3 = new Aluno(null,"Marcos Fabiano Reis", "1999-05-15",    "558833316", "1", "marcosfa@gmail.com","M");		
-		Aluno aluno4 = new Aluno(null,"Marcos Fabiano Reis", "1999-05-15",    "558833316", "1", "marcosfa@gmail.com","M");	
+		Aluno aluno1 = new Aluno(null, "Ana Luisa Aparecida", "2000-06-15", "1788891234", "1", "anareis@gmail.com","F");
+		//Aluno aluno2 = new Aluno(null, "Maria Joana Fagundes", "2000-10-11",   "201119224", "1", "mariaj2@gmail.com","F");
+		//Aluno aluno3 = new Aluno(null,"Marcos Fabiano Reis", "1999-05-15",    "558833316", "1", "marcosfa@gmail.com","M");		
+		//Aluno aluno4 = new Aluno(null,"Marcos Fabiano Reis", "1999-05-15",    "558833316", "1", "marcosfa@gmail.com","M");	
 		
 
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("projeto"); 
+		/*EntityManagerFactory emf = Persistence.createEntityManagerFactory("projeto"); 
 		EntityManager em = emf.createEntityManager(); 
 	
 		em.getTransaction().begin();
+		*/
 		
-		em.persist(aluno2); // chamo o gerenciador de tabelas (em) para fazer o comando insert realacionado ao aluno
-		em.persist(aluno3);
-		em.persist(aluno4);
+		//em.persist(aluno1); // chamo o gerenciador de tabelas (em) para fazer o comando insert realacionado ao aluno
+		//em.persist(aluno3);
+		//em.persist(aluno4);
 		
-		em.getTransaction().commit();
+		/*em.getTransaction().commit();
 
 		em.close(); 
 		emf.close();
+		*/
 
+		AlunoControle controle = new AlunoControle();
+		List<Aluno> objetos = controle.buscarTodos();
+		for (Iterator iterator = objetos.iterator(); iterator.hasNext();) {
+		Aluno obj = (Aluno) iterator.next();
+			System.out.println(obj.getNome());
 	}
 
+}
 }
