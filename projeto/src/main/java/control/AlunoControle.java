bed2a1d6d6c09ab14a0204f56ce2a7b0ebab5bfa
Dulcinha <https://github.com/Dulcinha>
2021-11-28 19:16:20 -0300

    Atividade A10 Etapa 3

--- projeto/src/main/java/control/AlunoControle.java
+++ projeto/src/main/java/control/AlunoControle.java
@@ -1,5 +1,29 @@
 package control;
 
-public class AlunoControle {
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+import model.Aluno;
+
+import javax.persistence.EntityManagerFactory;
+public class AlunoControle {
+	private EntityManager em;
+
+	public AlunoControle() {
+		EntityManager emf = (EntityManager) Persistence.createEntityManagerFactory("clinica");
+		em = ((EntityManagerFactory) emf).createEntityManager();
+	}
+	public void inserir(Aluno objeto) {}
+	public void alterar(Aluno objeto) {}
+	public void excluir(Aluno objeto) {}
+	public void excluirPorId(Integer id) {}
+	public Aluno buscarPorId(Integer id) {
+		return null;}
+	public List<Aluno> buscarTodos() {
+		return null;}
+	//outros métodos de busca que achar 
+	
+	
 }
